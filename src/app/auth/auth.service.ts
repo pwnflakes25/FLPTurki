74df7b15ce2b0b549a0d0c77ced8d89928d5a7c7
pwnflakes <pwnflakes25@gmail.com>
2020-07-19 02:38:03 +0300

    added signIn and currentUser method in AuthService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,38 +2,62 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import {AuthorService}
+import {Observer} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+$user: Observer<any>;
 
-  constructor(private auth: AngularFireAuth, private as: AuthorService) { }
+  constructor(private auth: AngularFireAuth, private as: AuthorService) {
+    this.auth.onAuthStateChanged(user =>  {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    })
 
+  }
+
+
+  getCurrentUser() {
+    return this.user;
+  }
 
   signUp(email, password, fullName, profileImage, about, company) {
-    this.auth.createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        // get user data from the auth trigger
-        const userUid = user.uid; // The UID of the user.
-
-        // set account  doc
-        const author: Author = {
-          id: userUid,
-          email: email,
-          fullName: fullName,
-          profileImageUrl: profileImage,
-          authorAbout: about,
-          company: company
-        }
-        this.as.createAuthor(userUid, author);
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // .
-    }
+        await this.auth.createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          // get user data from the auth trigger
+          const userUid = user.uid; // The UID of the user.
+          // set account  doc
+          const author: Author = {
+            id: userUid,
+            email: email,
+            fullName: fullName,
+            profileImageUrl: profileImage,
+            authorAbout: about,
+            company: company
+          }
+          this.as.createAuthor(userUid, author);
+        })
+        .catch(function(error) {
+         var errorCode = error.code;
+         var errorMessage = error.message;
+      });
   }
+
+  signIn(email, password) {
+    this.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      return error;
+    });
+  }
+
+
 
 }
--- src/app/blog/blog-edit/blog-edit.component.ts
+++ src/app/blog/blog-edit/blog-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { BlogService } from "../../blog.service";
+import {AuthService} from '../../auth/auth.service';
+import {Blog} from '../blog.model';
 
 @Component({
   selector: 'app-blog-edit',
@@ -9,7 +11,7 @@ import { BlogService } from "../../blog.service";
   styleUrls: ['./blog-edit.component.scss']
 })
 export class BlogEditComponent implements OnInit {
-
+blog: Blog;
 blogForm = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
@@ -22,9 +24,45 @@ blogForm = new FormGroup({
 });
 edit: boolean = false;
 
-  constructor(private bs: BlogService, private route: ActivatedRoute) { }
+  constructor(private bs: BlogService, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+    let id = params['id'];
+    let edit = params['edit'];
+
+      if (edit) {
+       this.setFormValue(id);
+      }
+
+    });
+  }
+
+  async setFormValue(id: string) {
+    this.blog = await this.bs.getBlogById(id);
+    this.blogForm.setValue({
+      title: this.blog.title,
+      description: this.blog.description,
+      content: this.blog.content,
+      summary: this.blog.summary,
+      imageUrl: this.blog.imageUrl,
+      authorId: this.blog.authorId,
+      date: this.blog.date,
+      tags: this.blog.tags
+    })
+  }
+
+  async onPostBlog() {
+    let user = await this.AuthService.getCurrentUser();
+    this.blogForm.patchValue({
+      authorId: user.id,
+      date: new Date()
+    })
+    this.bs.addBlog(this.blogForm.value);
+  }
+
+  onUpdateBlog() {
+    this.bs.updateBlog(this.blog.id, this.blogForm.value);
   }
 
 }
