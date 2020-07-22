42bac2b82ef9cfaca6623535bedba98c76e552e8
pwnflakes <pwnflakes25@gmail.com>
2020-07-22 19:03:30 +0300

    added details to env.prod

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+const path = require('path');
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+
+app.use(cors({origin: "*"}));
+app.use(bodyParser.json());
+app.use(forceSSL());
+
+
+
+// Start the app by listening on the default Heroku port
+app.listen( process.env.PORT || 8080);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/FLPTurki'));
+
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/FLPTurki/index.html'));
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+  apiKey: "AIzaSyAuWO1HpfxRCvd2D2ziGntzGgxX9sESLJE",
+  authDomain: "flpturki-cf280.firebaseapp.com",
+  databaseURL: "https://flpturki-cf280.firebaseio.com",
+  projectId: "flpturki-cf280",
+  storageBucket: "flpturki-cf280.appspot.com",
+  messagingSenderId: "1016701048240",
+  appId: "1:1016701048240:web:0ffb47672c9dbf581db275",
+  measurementId: "G-6TTH89VZJD"
+ }
 };
