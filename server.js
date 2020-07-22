adcc9fdd9344540c7ee3da90753a08eeae407bdf
pwnflakes <pwnflakes25@gmail.com>
2020-07-22 19:55:35 +0300

    added body-parser

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
 const app = express();
 const path = require('path');
 
