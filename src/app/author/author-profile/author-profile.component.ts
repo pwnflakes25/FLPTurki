31a293fd86550a7a70492fa911e3e87da3d6fa6d
pwnflakes <pwnflakes25@gmail.com>
2020-07-27 21:03:41 +0300

    fixed bug where nonsigned user can edit author blog

--- src/app/author/author-profile/author-profile.component.ts
+++ src/app/author/author-profile/author-profile.component.ts
@@ -7,6 +7,7 @@ import { Author } from "../author.model";
 import { ActivatedRoute } from '@angular/router';
 import {Observable, Subscription} from 'rxjs';
 import { filter, finalize } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-author-profile',
@@ -19,14 +20,19 @@ blogs: Blog[];
 author$: Observable<Author>;
 currentUserId: string;
 userSub: Subscription;
+urlAuthorId: string;
 isLoading = true;
-defaultProfilePic = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+defaultProfilePic = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+isEdit = false;
+accountForm: FormGroup;
+settingOption: string;
 
   constructor(private bs: BlogService, private as: AuthorService, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
     let id = params['id'];
+    this.urlAuthorId = id;
     this.bs.getBlogsByAuthorId().subscribe((res) => {
       this.blogs = res;
       this.isLoading = false;
@@ -40,7 +46,17 @@ defaultProfilePic = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profil
   }
 
   ngAfterViewInit(): void {
+  }
 
+
+  patchAccountForm(author: Author) {
+    this.accountForm = new FormGroup({
+      fullName: new FormControl(author.fullName, [Validators.required]),
+      email: new FormControl(author.email, [Validators.required]),
+      profileImageUrl: new FormControl(author.profileImageUrl, [Validators.required]),
+      authorAbout: new FormControl(author.authorAbout, [Validators.required]),
+      company: new FormControl(author.company, [Validators.required])
+    })
   }
 
   onDelete(authorId: string, blogId: string) {
@@ -52,6 +68,17 @@ defaultProfilePic = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profil
         alert(error);
       }
     }
+  }
+
+  onSave() {
+    try {
+      this.as.updateAuthorProfile(this.currentUserId, this.accountForm.value);
+      alert('Change Saved');
+      this.isEdit = false;
+    } catch (error) {
+      alert(error);
+    }
+
   }
 
   ngOnDestroy(): void {
